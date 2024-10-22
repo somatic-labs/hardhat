0f8b8b481259c8787a43cf69b0710bd9eb5ca620
Jacob Gadikian <jacobgadikian@gmail.com>
2024-10-22 16:15:31 +0700

    add grpc client library

---
+++ client/grpc_client.go
@@ -0,0 +1,51 @@
+package client
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	tx "github.com/cosmos/cosmos-sdk/types/tx"
+)
+
+type GRPCClient struct {
+	conn     *grpc.ClientConn
+	txClient tx.ServiceClient
+}
+
+func NewGRPCClient(grpcEndpoint string) (*GRPCClient, error) {
+	conn, err := grpc.Dial(grpcEndpoint, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+
+	txClient := tx.NewServiceClient(conn)
+
+	return &GRPCClient{
+		conn:     conn,
+		txClient: txClient,
+	}, nil
+}
+
+func (c *GRPCClient) SendTx(ctx context.Context, txBytes []byte) (*sdk.TxResponse, error) {
+	grpcRes, err := c.txClient.BroadcastTx(
+		ctx,
+		&tx.BroadcastTxRequest{
+			Mode:    tx.BroadcastMode_BROADCAST_MODE_SYNC,
+			TxBytes: txBytes,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return grpcRes.TxResponse, nil
+}
+
+func (c *GRPCClient) Close() {
+	if c.conn != nil {
+		c.conn.Close()
+	}
+}
